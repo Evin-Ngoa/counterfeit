14e25b9eb592be81c6eb750ede8d2a93c5c75576
Evin <toneevin07@gmail.com>
2020-02-16 22:57:08 +0300

    Added telephone data into table for Distributors

--- resources/views/distributors/index.blade.php
+++ resources/views/distributors/index.blade.php
@@ -29,39 +29,45 @@
                                     <th class="text-center">Name</th>
                                     <th class="text-center">Email</th>
                                     <th class="text-center">Member ID</th>
+                                    <th class="text-center">Tel</th>
                                     <th class="text-center">Address</th>
                                     <th class="text-center">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 @if(!empty($distributors))
-                                    @foreach($distributors as $distributor)
-                                    <tr>
-                                        <!-- <td class="text-center">
+                                @foreach($distributors as $distributor)
+                                <tr>
+                                    <!-- <td class="text-center">
                                             <input class="form-control" type="checkbox">
                                         </td> -->
-                                        <td>
-                                            <div class="user-with-avatar">
-                                                <img alt="" src="img/avatar1.jpg">
-                                                <span class="smaller">{{ $distributor->name }}</span>
-                                            </div>
-                                        </td>
-                                        <td>
-                                            <div class="smaller text-center">{{ $distributor->email }}</div>
-                                        </td>
-                                        <td><span class="smaller text-center">{{ $distributor->memberId }}</span></td>
-                                        <td>
-                                            <span class="smaller text-center">
-                                                {{ $distributor->address->country }},
-                                                {{ $distributor->address->county }},
-                                                {{ $distributor->address->street }}
-                                            </span>
-                                        </td>
-                                        <td class="row-actions">
-                                            <a href="#" data-placement="top" data-toggle="tooltip" title="Track Book"><i class="os-icon os-icon-truck"></i></a><a onclick="event.preventDefault();editDistributorForm('{{ $distributor->email }}');" href="#" data-placement="top" data-toggle="tooltip" title="Edit"><i class="os-icon os-icon-edit"></i></a><a class="danger" onclick="event.preventDefault();deleteDistributorForm('{{ $distributor->email }}');" href="#" data-placement="top" data-toggle="tooltip" title="Delete"><i class="os-icon os-icon-ui-15"></i></a>
-                                        </td>
-                                    </tr>
-                                    @endforeach
+                                    <td>
+                                        <div class="user-with-avatar">
+                                            <img alt="" src="img/avatar1.jpg">
+                                            <span class="smaller">{{ $distributor->name }}</span>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        <div class="smaller text-center">{{ $distributor->email }}</div>
+                                    </td>
+                                    <td><span class="smaller text-center">{{ $distributor->memberId }}</span></td>
+                                    @if(isset( $publisher->telephone ))
+                                    <td><span class="smaller text-center">{{ $publisher->telephone }}</span></td>
+                                    @else
+                                    <td><span class="smaller text-center">Unknown</span></td>
+                                    @endif
+                                    <td>
+                                        <span class="smaller text-center">
+                                            {{ $distributor->address->country }},
+                                            {{ $distributor->address->county }},
+                                            {{ $distributor->address->street }}
+                                        </span>
+                                    </td>
+                                    <td class="row-actions">
+                                        <a href="#" data-placement="top" data-toggle="tooltip" title="Track Book"><i class="os-icon os-icon-truck"></i></a><a onclick="event.preventDefault();editDistributorForm('{{ $distributor->email }}');" href="#" data-placement="top" data-toggle="tooltip" title="Edit"><i class="os-icon os-icon-edit"></i></a><a class="danger" onclick="event.preventDefault();deleteDistributorForm('{{ $distributor->email }}');" href="#" data-placement="top" data-toggle="tooltip" title="Delete"><i class="os-icon os-icon-ui-15"></i></a>
+                                    </td>
+                                </tr>
+                                @endforeach
                                 @else
                                 <tr>
                                     <div class="alert alert-info" role="alert">
