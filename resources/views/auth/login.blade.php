85b9aad2b0f9bccbf47980059e6ee343abcffaab
Evin <toneevin07@gmail.com>
2020-03-01 23:24:33 +0300

    Added Scripts At the Footer Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -37,23 +37,23 @@
 <body class="auth-wrapper">
     <div class="all-wrapper menu-side with-pattern">
         <div class="auth-box-w">
-            <div class="logo-w">
+            <div class="logo-w" style="padding: 5%;">
                 <a href="#"><img alt="" src="/img/logo-big.png"></a>
             </div>
             <h4 class="auth-header">Login Form</h4>
-            <form action="#">
+            <form id="frmLogin">
                 <div class="form-group">
                     <label for="">Username</label>
-                    <input class="form-control" placeholder="Enter your username">
+                    <input type="text" class="form-control" name="username" id="username" placeholder="Enter your username">
                     <div class="pre-icon os-icon os-icon-user-male-circle"></div>
                 </div>
                 <div class="form-group">
                     <label for="">Password</label>
-                    <input class="form-control" placeholder="Enter your password" type="password">
+                    <input type="password" name="password" id="password" class="form-control" placeholder="Enter your password" >
                     <div class="pre-icon os-icon os-icon-fingerprint"></div>
                 </div>
                 <div class="buttons-w">
-                    <button class="btn btn-primary">Log me in</button>
+                    <button class="btn btn-primary btn-login" type="button">Log me in</button>
                     <div class="form-check-inline">
                         <label class="form-check-label">
                             <input class="form-check-input" type="checkbox">Remember Me</label>
@@ -62,6 +62,46 @@
             </form>
         </div>
     </div>
+
+    <script src="{{ asset ("vendor/jquery/dist/jquery.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset ("vendor/popper.js/dist/umd/popper.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset ("vendor/moment/moment.js") }}" type="text/javascript"></script>
+    <script src="{{ asset ("vendor/chart.js/dist/Chart.min.js") }}" type="text/javascript"></script>
+    <script src="{{ asset ("vendor/select2/dist/js/select2.full.min.js") }}"></script>
+    <script src="{{ asset ("vendor/jquery-bar-rating/dist/jquery.barrating.min.js") }}"></script>
+    <script src="{{ asset ("vendor/ckeditor/ckeditor.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap-validator/dist/validator.min.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap-daterangepicker/daterangepicker.js") }}"></script>
+    <script src="{{ asset ("vendor/ion.rangeSlider/js/ion.rangeSlider.min.js") }}"></script>
+    <script src="{{ asset ("vendor/dropzone/dist/dropzone.js") }}"></script>
+    <script src="{{ asset ("vendor/editable-table/mindmup-editabletable.js") }}"></script>
+    <script src="{{ asset ("vendor/datatables.net/js/jquery.dataTables.min.js") }}"></script>
+    <script src="{{ asset ("vendor/datatables.net-bs/js/dataTables.bootstrap.min.js") }}"></script>
+    <script src="{{ asset ("vendor/fullcalendar/dist/fullcalendar.min.js") }}"></script>
+    <script src="{{ asset ("vendor/perfect-scrollbar/js/perfect-scrollbar.jquery.min.js") }}"></script>
+    <script src="{{ asset ("vendor/tether/dist/js/tether.min.js") }}"></script>
+    <script src="{{ asset ("vendor/slick-carousel/slick/slick.min.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/util.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/alert.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/button.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/carousel.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/collapse.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/dropdown.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/modal.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/tab.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/tooltip.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap/js/dist/popover.js") }}"></script>
+    <script src="{{ asset ("vendor/bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js") }}"></script>
+    <script src="{{ asset ("js/dataTables.bootstrap4.min.js") }}"></script>
+    <script src="{{ asset ("js/demo_customizer5739.js") }}"></script>
+    <script src="{{ asset ("js/main5739.js") }}"></script>
+    <script src="{{ asset ("js/api.js") }}"></script>
+    <script src="{{ asset ("js/helper-api.js") }}"></script>
+
+    <script>
+        Api.init();
+
+    </script>
 </body>
 
 </html>
