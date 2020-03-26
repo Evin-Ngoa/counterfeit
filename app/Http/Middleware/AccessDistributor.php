70b2202d380db4c2c4c559e944fb918b954534cb
Evin <toneevin07@gmail.com>
2020-03-27 00:44:43 +0300

    Distributor Middleware

---
+++ app/Http/Middleware/AccessDistributor.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Traits\UserConstants;
+use App\User;
+use Closure;
+
+class AccessDistributor
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if the logged in is an DISTRIBUTOR
+        if (User::getUserRole() == UserConstants::DISTRIBUTOR) {
+            return $next($request);
+        }
+        return redirect('book');
+    }
+}
