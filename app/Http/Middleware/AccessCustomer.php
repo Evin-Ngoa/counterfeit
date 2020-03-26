b171844034ee667a3f2218684701895f5f61f239
Evin <toneevin07@gmail.com>
2020-03-27 00:44:24 +0300

    Customer Middleware

---
+++ app/Http/Middleware/AccessCustomer.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Traits\UserConstants;
+use App\User;
+use Closure;
+
+class AccessCustomer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if the logged in is an CUSTOMER
+        if (User::getUserRole() == UserConstants::CUSTOMER) {
+            return $next($request);
+        }
+        return redirect('book');
+    }
+}
