ce688f69dd63e84d22511c3572cc0678832b0327
Evin <toneevin07@gmail.com>
2020-03-27 16:55:43 +0300

    Register Different User Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,6 +61,10 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'auth.admin' => \App\Http\Middleware\AccessAdmin::class,
+        'auth.publisher' => \App\Http\Middleware\AccessPublisher::class,
+        'auth.distributor' => \App\Http\Middleware\AccessDistributor::class,
+        'auth.customer' => \App\Http\Middleware\AccessCustomer::class,
+        'check.auth' => \App\Http\Middleware\CheckAuth::class,
     ];
 
     /**
