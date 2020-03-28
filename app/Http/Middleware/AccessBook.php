781340970b9cb25a3d2d44e626100bfd3a9fbf00
Evin <toneevin07@gmail.com>
2020-03-28 22:17:39 +0300

    Created middleware to check only allowed users to view book related information

---
+++ app/Http/Middleware/AccessBook.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Traits\UserConstants;
+use App\User;
+use Closure;
+
+class AccessBook
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+         // Check if the logged in is an Admin OR Publisher
+         if(User::getUserRole() == UserConstants::ADMIN || User::getUserRole() == UserConstants::PUBLISHER){
+            return $next($request);
+        }
+        return redirect('/auth/login');
+    }
+}
