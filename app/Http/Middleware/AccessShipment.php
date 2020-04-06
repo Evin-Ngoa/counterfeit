548dfbcb1f045392e1bdb6923d4dc2c1a5746102
Evin <toneevin07@gmail.com>
2020-04-06 03:21:31 +0300

    Added Middleware for users that can  only access Shipments

---
+++ app/Http/Middleware/AccessShipment.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Http\Traits\UserConstants;
+use App\User;
+use Closure;
+
+class AccessShipment
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if the logged in is an Admin OR Publisher
+        if (
+            User::getUserRole() == UserConstants::ADMIN ||
+            User::getUserRole() == UserConstants::PUBLISHER ||
+            User::getUserRole() == UserConstants::DISTRIBUTOR ||
+            User::getUserRole() == UserConstants::CUSTOMER
+        ) {
+            return $next($request);
+        }
+        return redirect('dashboard');
+    }
+}
