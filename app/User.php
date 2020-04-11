91dfbfbc6deb55e9814725327c6f8a222f379e03
Evin <toneevin07@gmail.com>
2020-04-12 01:25:27 +0300

    Check remote user if exiata in checkAuth for checking logged in User

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use App\Http\Traits\UserConstants;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Passport\HasApiTokens;
@@ -77,10 +78,62 @@ class User extends Authenticatable
      * Check if a user is loggedin
      */
     public static function checkAuth(){
+        // if someone loggedin
         if (isset($_COOKIE['logged_in_user'])) {
-            return true;
+            $userEmail = self::loggedInUserEmail();
+            // $userEmail = "publisher1@gmail.com";
+            $ifUserExistsBool = self::checkUser($userEmail);
+            // dd($ifUserExistsBool);
+            return $ifUserExistsBool;
         }
         return false;
+    }
+
+    /**
+     * Check if remote user exists
+     * in all participants
+     */
+    public static function checkUser($userEmail){
+        $bool = false;
+        $role = self::getUserRole();
+        // $role = UserConstants::PUBLISHER;
+
+        if($role == UserConstants::ADMIN){
+            $bool = self::remoteUserCall(UserConstants::ADMIN, $userEmail);
+        }
+
+        if($role == UserConstants::CUSTOMER){
+            $bool = self::remoteUserCall(UserConstants::CUSTOMER, $userEmail);
+        }
+
+        if($role == UserConstants::PUBLISHER){
+            $bool = self::remoteUserCall(UserConstants::PUBLISHER, $userEmail);
+        }
+
+        if($role == UserConstants::DISTRIBUTOR){
+            $bool = self::remoteUserCall(UserConstants::DISTRIBUTOR, $userEmail);
+        }
+
+        return $bool;
+
+    }
+
+    /**
+     * Remote Api Call
+     */
+    public static function remoteUserCall($participant ,$userEmail){
+        // Fetch remotely to confirm the user
+        // coz the cookie might not expire but the remote user 
+        // does not exist
+        $get_data = self::callAPI('GET', 'http://localhost:3001/api/' . $participant.'/' .$userEmail, false);
+        $response = json_decode($get_data, true);
+        // dd($response);
+        // $errors = $response['error']['statusCode'];
+        if(isset($response['error']['statusCode'])){
+            return false;
+        }else{
+            return true;
+        }
     }
 
     /*
@@ -128,4 +181,36 @@ class User extends Authenticatable
         // Use -1 to get the index.
         return $split[count($split) - 1];
     }
+
+    public static function callAPI($method, $url, $data){
+        $curl = curl_init();
+        switch ($method){
+           case "POST":
+              curl_setopt($curl, CURLOPT_POST, 1);
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+              break;
+           case "PUT":
+              curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "PUT");
+              if ($data)
+                 curl_setopt($curl, CURLOPT_POSTFIELDS, $data);			 					
+              break;
+           default:
+              if ($data)
+                 $url = sprintf("%s?%s", $url, http_build_query($data));
+        }
+        // OPTIONS:
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+           'APIKEY: 111111111111111111111',
+           'Content-Type: application/json',
+        ));
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        // EXECUTE:
+        $result = curl_exec($curl);
+        if(!$result){die("Connection Failure");}
+        curl_close($curl);
+        return $result;
+     }
 }
