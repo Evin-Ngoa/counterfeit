4cfaa43c1cce7faecca5a9e712f20c38532cf1cd
Evin <toneevin07@gmail.com>
2020-04-12 03:54:50 +0300

    Corrected middleware for access for different users, view for each user to view their only shipments, view for show

--- app/Http/Controllers/ShipmentController.php
+++ app/Http/Controllers/ShipmentController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Services\Shipment\ShipmentService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
 
 class ShipmentController extends Controller
 {
@@ -12,6 +13,10 @@ class ShipmentController extends Controller
     public function __construct(ShipmentService  $shipmentservice)
     {
         $this->shipmentservice = $shipmentservice;
+
+        $this->middleware('check.auth');
+        $this->middleware('auth.admin')->only('index');
+        $this->middleware('auth.shipment');
     }
     /**
      * Display a listing of the resource.
@@ -24,6 +29,65 @@ class ShipmentController extends Controller
         $shipments = $this->shipmentservice->getAllShipments();
         // dd($shipments);
 
+        return view('shipments.index')->with(compact('shipments'));
+    }
+
+    /**
+     * Checking Owner Shipments
+     */
+    public function view_shipments($id)
+    {
+
+        $isSame = \App\User::active($id);
+
+        if (!$isSame) {
+            // dd($isSame);
+            $userEmail = \App\User::loggedInUserEmail();
+            // dd($userEmail);
+            return Redirect::route('shipment.view', [$userEmail]);
+        }
+
+        $shipment = $this->shipmentservice->getAllShipments();
+
+        $shipments = array();
+        $array = array();
+
+        if (\App\User::getUserRole() == \App\Http\Traits\UserConstants::PUBLISHER) {
+            foreach ($shipment as $key => $value) {
+                    
+                if ($shipment[$key]->contract->seller->email == $id) {
+                    $array[ $key ] = $value;
+                    $shipments = $array;
+                }
+
+            }
+        } else if(\App\User::getUserRole() == \App\Http\Traits\UserConstants::CUSTOMER){
+            foreach ($shipment as $key => $value) {
+
+                if ($shipment[$key]->contract->buyer->email == $id) {
+                    $array[ $key ] = $value;
+                    $shipments = $array;
+                }
+            }
+        } else if(\App\User::getUserRole() == \App\Http\Traits\UserConstants::DISTRIBUTOR){
+            // dd($shipment);
+            
+            foreach ($shipment as $key => $value) {
+                // dd(count($shipment[$key]->shipOwnership));
+                // if the array has owners
+                if(count($shipment[$key]->shipOwnership) > 0){
+                    // loop shipowners
+                    foreach ($shipment[$key]->shipOwnership as $keys => $values) {
+                        if ($shipment[$key]->shipOwnership[$keys]->owner->email == $id) {
+                            $array[ $key ] = $value;
+                            $shipments = $array;
+                        }
+                    }
+                }
+                
+            }
+        }
+        // dd($shipments );
         return view('shipments.index')->with(compact('shipments'));
     }
 
@@ -57,6 +121,12 @@ class ShipmentController extends Controller
     public function show($id)
     {
         //
+        $shipment = $this->shipmentservice->getSingleShipment($id);
+
+        return response()->json([
+            'error' => false,
+            'shipment'  => $shipment,
+        ], 200);
     }
 
     /**
@@ -67,7 +137,12 @@ class ShipmentController extends Controller
      */
     public function edit($id)
     {
-        //
+        $shipment = $this->shipmentservice->getSingleShipment($id);
+
+        return response()->json([
+            'error' => false,
+            'shipment'  => $shipment,
+        ], 200);
     }
 
     /**
