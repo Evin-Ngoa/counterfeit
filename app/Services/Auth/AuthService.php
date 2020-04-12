53314ff0b767ead51b221467eea12d129ecba61e
Evin <toneevin07@gmail.com>
2020-04-12 03:51:52 +0300

    Authentication Service though not used currently

---
+++ app/Services/Auth/AuthService.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Services\Auth;
+
+use App\Services\Utils;
+
+class AuthService
+{
+    protected $utils;
+
+	public function __construct(Utils $utils)
+	{
+		$this->utils = $utils;
+    }
+    /**
+     * Get all Users Records
+     */
+    public function getAllAdmins(){
+        return $this->utils->all('/Admin');
+    }
+
+    /**
+     * Get Single User 
+     */
+    public function getSingleUser($id){
+        $admin = $this->utils->findById('/Admin/', $id);
+        $customer = $this->utils->findById('/Customer/', $id);
+        $distributor = $this->utils->findById('/Distributor/', $id);
+        $publisher = $this->utils->findById('/Publisher/', $id);
+        dd("admin => ". $admin . "customer => " . $customer . "distributor => " . $distributor . "publisher => " . $publisher);
+        // return $this->utils->findById('/Distributor/', $id);
+    }
+   
+}
