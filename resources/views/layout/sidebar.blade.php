016bf7212bde523177082b8726e460f72f5ce43a
Evin <toneevin07@gmail.com>
2020-04-20 01:01:24 +0300

    Updated sid menu links including profile link

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -26,7 +26,7 @@
                 <div class="bg-icon"><i class="os-icon os-icon-wallet-loaded"></i></div>
                 <ul>
                     <li><a href="apps_email.html"><i class="os-icon os-icon-mail-01"></i><span>Incoming Mail</span></a></li>
-                    <li><a href="users_profile_big.html"><i class="os-icon os-icon-user-male-circle2"></i><span>Profile Details</span></a></li>
+                    <li><a href="{{ route('profile.index') }}"><i class="os-icon os-icon-user-male-circle2"></i><span>Profile Details</span></a></li>
                     <li><a href="users_profile_small.html"><i class="os-icon os-icon-coins-4"></i><span>Billing Details</span></a></li>
                     <li><a href="#"><i class="os-icon os-icon-others-43"></i><span>Notifications</span></a></li>
                     <li><a href="#" onclick="event.preventDefault();UnSetToken();"><i class="os-icon os-icon-signs-11"></i><span>Logout</span></a></li>
@@ -37,8 +37,15 @@
 
     <h1 class="menu-page-header">Page Header</h1>
     <ul class="main-menu">
-        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
-        <li class="sub-header"><span>Menu</span></li>
+    <li class="sub-header"><span>Menu</span></li>
+        <li class="selected {!! classActivePath('dashboard') !!}">
+            <a href="{{ route('dashboard.index') }}">
+                <div class="icon-w">
+                    <div class="os-icon os-icon-grid-circles"></div>
+                </div><span>Dashboard</span>
+            </a>
+        </li>
+        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER)
         <li class="selected {!! classActivePath('book') !!}">
             <a href="{{ route('book.view', ['id' => \App\User::loggedInUserEmail()]) }}">
                 <div class="icon-w">
@@ -47,6 +54,28 @@
             </a>
         </li>
         @endif
+        
+        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
+        <li class="selected {!! classActivePath('book') !!}">
+            <a href="{{ route('book.index') }}">
+                <div class="icon-w">
+                    <div class="os-icon os-icon-book"></div>
+                </div><span>Book</span>
+            </a>
+        </li>
+        @endif
+
+        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::CUSTOMER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
+        <li class="selected {!! classActivePath('verify') !!}">
+            <a href="{{ route('verify.form') }}">
+                <div class="icon-w">
+                    <div class="os-icon os-icon-share-2"></div>
+                </div><span>Verify Book</span>
+            </a>
+        </li>
+        @endif
+
+
         <li class="sub-header"><span>Activities</span></li>
         @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::CUSTOMER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
         <li class="selected {!! classActivePath('order') !!}">
@@ -57,9 +86,9 @@
             </a>
         </li>
         @endif
-        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::DISTRIBUTOR || \App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
+        @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::PUBLISHER || \App\User::getUserRole()==\App\Http\Traits\UserConstants::DISTRIBUTOR || \App\User::getUserRole()==\App\Http\Traits\UserConstants::CUSTOMER)
         <li class="selected {!! classActivePath('shipment') !!}">
-            <a href="{{ route('shipment.index') }}">
+            <a href="{{ route('shipment.view',  ['id' => \App\User::loggedInUserEmail()]) }}">
                 <div class="icon-w">
                     <div class="os-icon os-icon-globe"></div>
                 </div><span>Shipments</span>
@@ -67,6 +96,13 @@
         </li>
         @endif
         @if(\App\User::getUserRole()==\App\Http\Traits\UserConstants::ADMIN)
+        <li class="selected {!! classActivePath('shipment') !!}">
+            <a href="{{ route('shipment.index') }}">
+                <div class="icon-w">
+                    <div class="os-icon os-icon-globe"></div>
+                </div><span>Shipments</span>
+            </a>
+        </li>
         <li class="sub-header"><span>Users</span></li>
         <li class="selected {!! classActivePath('publisher') !!}">
             <a href="{{ route('publisher.index') }}">
