a04f647eab835e82ebe789d4cf42f18adf4017a6
Evin <toneevin07@gmail.com>
2020-04-21 23:34:41 +0300

    Added function to ProfileController to get profile detials and upload profile picture

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,15 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Traits\OrderConstants;
+use App\Http\Traits\UserConstants;
+use App\Services\Book\BookService;
+use App\Services\Customer\CustomerService;
+use App\Services\Distributor\DistributorService;
+use App\Services\Order\OrderService;
+use App\Services\Publisher\PublisherService;
+use App\Services\Shipment\ShipmentService;
+use App\Services\Utils;
 use App\User;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
+    protected $bookservice;
+    protected $orderservice;
+    protected $shipmentservice;
+    protected $customerservice;
+    protected $publisherservice;
+    protected $distributorservice;
+
     /**
      * Check if user is loggedin
      */
-    public function __construct() {
+    public function __construct(
+        Utils $utils,
+        BookService $bookservice,
+        OrderService  $orderservice,
+        ShipmentService $shipmentservice,
+        CustomerService $customerservice,
+        PublisherService $publisherservice,
+        DistributorService $distributorservice
+    ) {
+        $this->utils = $utils;
+        $this->bookservice = $bookservice;
+        $this->orderservice = $orderservice;
+        $this->shipmentservice = $shipmentservice;
+        $this->customerservice = $customerservice;
+        $this->publisherservice = $publisherservice;
+        $this->distributorservice = $distributorservice;
         $this->middleware('check.auth');
     }
 
@@ -24,7 +55,153 @@ class ProfileController extends Controller
         $authEmail = User::loggedInUserEmail();
         $authRole = User::getUserRole();
 
+        if (\App\User::getUserRole() == \App\Http\Traits\UserConstants::PUBLISHER) {
+            // BOOKS COUNT
+            $booksPub = $this->bookservice->getPublisherBooks($authEmail);
+            $booksPubCount = count($booksPub);
+
+            // DELIVER COUNT
+            $shipments = array();
+            $shipmentPub = $this->shipmentservice->getAllShipments();
+
+            foreach ($shipmentPub as $key => $value) {
+                // Active Shipments include waiting, dispatching and transit
+                if ($shipmentPub[$key]->contract->seller->email == \App\User::loggedInUserEmail() && ($shipmentPub[$key]->ShipmentStatus == OrderConstants::SHIP_DELIVERED)) {
+                    $arrayShip[$key] = $value;
+                    $shipments = $arrayShip;
+                }
+            }
+            $shipmentsCount = count($shipments);
+
+            return view('profile.index')->with(compact('authRole', 'authEmail', 'booksPubCount', 'shipmentsCount'));
+        }else if(\App\User::getUserRole() == \App\Http\Traits\UserConstants::DISTRIBUTOR){
+            // 1. Active Orders
+            $shipments = array();
+
+            $shipmentDistr = $this->shipmentservice->getAllShipments();
+
+            foreach ($shipmentDistr as $key => $value) {
+                // dd(count($shipment[$key]->shipOwnership));
+                // if the array has owners and its still processing
+                if(count($shipmentDistr[$key]->shipOwnership) > 0  && 
+                $shipmentDistr[$key]->ShipmentStatus == (OrderConstants::SHIP_WAITING || $shipmentPub[$key]->ShipmentStatus == OrderConstants::SHIP_DISPATCHING || $shipmentPub[$key]->ShipmentStatus == OrderConstants::SHIP_SHIPPED_IN_TRANSIT)){
+                    // loop shipowners
+                    foreach ($shipmentDistr[$key]->shipOwnership as $keys => $values) {
+                        if ($shipmentDistr[$key]->shipOwnership[$keys]->owner->email == $authEmail) {
+                            $array[ $key ] = $value;
+                            $shipments = $array;
+                        }
+                    }
+                }
+                
+            }
+            // dd($shipments);
+            $shipmentsActiveCount = count($shipments);
+
+            // 2. Delivery
+            $shipmentsDelivered = array();
+
+            $shipmentDistr = $this->shipmentservice->getAllShipments();
+
+            foreach ($shipmentDistr as $key => $value) {
+                // dd(count($shipment[$key]->shipOwnership));
+                // if the array has owners and its still processing
+                if(count($shipmentDistr[$key]->shipOwnership) > 0  && 
+                $shipmentDistr[$key]->ShipmentStatus == OrderConstants::SHIP_DELIVERED){
+                    // loop shipowners
+                    foreach ($shipmentDistr[$key]->shipOwnership as $keys => $values) {
+                        if ($shipmentDistr[$key]->shipOwnership[$keys]->owner->email == $authEmail) {
+                            $array[ $key ] = $value;
+                            $shipmentsDelivered = $array;
+                        }
+                    }
+                }
+                
+            }
+            // dd($shipmentsDelivered);
+            $shipmentsDeliveredCount = count($shipmentsDelivered);
+
+            return view('profile.index')->with(compact('authRole', 'authEmail','shipmentsActiveCount','shipmentsDeliveredCount'));
+
+        }else if(\App\User::getUserRole() == \App\Http\Traits\UserConstants::CUSTOMER){
+            // 1. ACTIVE ORDERS
+            $orders = $this->orderservice->getOnlyUserOrders($authEmail, 'buyer', \App\Http\Traits\UserConstants::CUSTOMER);
+            $activeOrders = array();
+            
+            foreach ($orders as $key => $value) {
+                // Active Orders include ; Sum(waiting, processed)  
+                if ($orders[$key]->orderStatus == OrderConstants::WAITING || $orders[$key]->orderStatus == OrderConstants::PROCESSED) {
+                    $array[$key] = $value;
+                    $activeOrders = $array;
+                }
+            }
+
+            // dd($activeOrders);
+            $ordersActiveCount = count($activeOrders);
+
+            // 1. DELIVERED ORDERS
+            $deliveredOrders = array();
+
+            foreach ($orders as $key => $value) {
+                // Active Orders include ; Sum(waiting, processed)  
+                if ($orders[$key]->orderStatus == OrderConstants::SHIP_DELIVERED) {
+                    $array[$key] = $value;
+                    $deliveredOrders = $array;
+                }
+            }
+
+            $deliveredOrdersCount = count($deliveredOrders);
+
+            return view('profile.index')->with(compact('authRole', 'authEmail', 'ordersActiveCount', 'deliveredOrdersCount'));
+
+        }
+
         return view('profile.index')->with(compact('authRole', 'authEmail'));
+    }
+
+    /**
+     * Upload user profile
+     */
+    public function update_avatar(Request $request){
+
+        $authEmail = User::loggedInUserEmail();
+        $authRole = User::getUserRole();
+        $picPath = '/images/';
+
+        $avatarName = User::getParticipantMemberID() .'_avatar_'. time() .request()->avatar->getClientOriginalExtension();
+        
+        if ($files = $request->file('avatar')) {
+            $files->move( public_path() . '/images/', $avatarName);
+        }
+        
+        if($authRole == UserConstants::CUSTOMER){
+            $data = json_encode(array('avatar' => $picPath . $avatarName , "customer" => "resource:org.evin.book.track.".$authRole."#". $authEmail ));   
+        }else if($authRole == UserConstants::PUBLISHER){
+            $data = json_encode(array('avatar' => $picPath .$avatarName , "publisher" => "resource:org.evin.book.track.".$authRole."#". $authEmail ));
+        }else if($authRole == UserConstants::DISTRIBUTOR){
+            $data = json_encode(array('avatar' => $picPath .$avatarName , "distributor" => "resource:org.evin.book.track.".$authRole."#". $authEmail ));
+        }
+ 
+        $get_data = User::callAPI('POST', 'http://localhost:3001/api/' .'upload'. $authRole .'ProfilePic', $data);
+        $response = json_decode($get_data, true);
+
+        if(isset($response['error']['statusCode'])){
+            $message =  $response['error']['message'];
+
+            return Response()->json([
+                "success" => false,
+                "image" => '',
+                "message" => $message
+            ]);
+        }else{
+            $message = "Profile Image Uploaded Successfully";
+        }
+        return Response()->json([
+            "success" => true,
+            "image" => '',
+            "message" => $message
+        ]);
+
     }
 
     /**
