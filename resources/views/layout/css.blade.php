cfd515e53a55c7904c2bff80d92bcb1e1abe4119
Evin <toneevin07@gmail.com>
2020-06-08 00:08:45 +0300

    Added thecss for icon for love

--- resources/views/layout/css.blade.php
+++ resources/views/layout/css.blade.php
@@ -1,1 +1,2 @@
     <link href="{{ asset("css/app-bc.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("icon_fonts_assets/typicons/typicons.css") }}" rel="stylesheet" type="text/css" />
