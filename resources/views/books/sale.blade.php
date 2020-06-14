e07ab2e3ae645f50741584c78181a577817c0cca
Evin <toneevin07@gmail.com>
2020-06-15 01:03:29 +0300

    Separated buy book form in include book-verify

---
+++ resources/views/books/sale.blade.php
@@ -0,0 +1,36 @@
+<div class="element-wrapper">
+    <h6 class="element-header">Buy Book</h6>
+    <div class="element-box">
+        <form id="frmBuyBook">
+            <!-- <div class="alert alert-warning">
+                Report a book that is rendered possible counterfeit and get points. However false alarm in order to get
+                points can lead to permanent disqualification of earning points or penalty deductions depending on the
+                frequency of false alarm.
+            </div> -->
+            <div id="add-buy-book-msgs"></div>
+
+            <div class="alert alert-danger" id="add-error-buy-book-bag" style="display: none;">
+                <ul id="add-buy-book-errors">
+                </ul>
+            </div>
+
+
+            <div class="form-group">
+                <label for="book"> Book ID</label>
+                <!-- <input class="form-control" placeholder="Enter Book ID" type="text" name="book_show" id="book_show" value="{{ $id }}" disabled> -->
+                <input class="form-control" placeholder="Enter Book ID" type="text" name="book" id="book" value="{{ $id }}">
+            </div>
+
+            <div class="form-group">
+                <label for="store">Purchase From [BookShop ID]</label>
+                <input class="form-control" type="text" name="purchasedToMemberId" id="purchasedToMemberId" value="{{$reportedToMemberId}}" >
+                <input class="form-control" type="hidden" name="purchasedTo" id="purchasedTo" value="{{$purchasedToEmail}}">
+                <input class="form-control" type="hidden" name="purchasedBy" id="purchasedBy" value="{{\App\User::loggedInUserEmail()}}">
+            </div>
+
+            <div class="form-buttons-w">
+                <button class="btn btn-primary btn-buy-book" type="button"> Purchase Book</button>
+            </div>
+        </form>
+    </div>
+</div>
