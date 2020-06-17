851b3b3b5fb24ae18d8714660b761ab9ad5993af
Evin <toneevin07@gmail.com>
2020-06-17 22:25:31 +0300

    Deleted unnecessary html code and commented the content-panel

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <!-- <title>Admin Dashboard HTML Template</title> -->
     <title>@yield('title', config('app.name', 'en'))</title>
@@ -26,158 +27,7 @@
 
 <body class="menu-position-side menu-side-left full-screen with-content-panel">
     <div class="all-wrapper with-side-panel solid-bg-all">
-        <div aria-hidden="true" class="onboarding-modal modal fade animated show-on-load" role="dialog" tabindex="-1" style="display:none;">
-            <div class="modal-dialog modal-centered" role="document">
-                <div class="modal-content text-center">
-                    <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span class="close-label">Skip Intro</span><span class="os-icon os-icon-close"></span></button>
-                    <div class="onboarding-slider-w">
-                        <div class="onboarding-slide">
-                            <div class="onboarding-media"><img alt="" src="/img/bigicon2.png" width="200px"></div>
-                            <div class="onboarding-content with-gradient">
-                                <h4 class="onboarding-title">Example of onboarding screen!</h4>
-                                <div class="onboarding-text">This is an example of a multistep onboarding screen, you can use it to introduce your customers to your app, or collect additional information from them before they start using your app.</div>
-                            </div>
-                        </div>
-                        <div class="onboarding-slide">
-                            <div class="onboarding-media"><img alt="" src="/img/bigicon5.png" width="200px"></div>
-                            <div class="onboarding-content with-gradient">
-                                <h4 class="onboarding-title">Example Request Information</h4>
-                                <div class="onboarding-text">In this example you can see a form where you can request some additional information from the customer when they land on the app page.</div>
-                                <form>
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label for="">Your Full Name</label>
-                                                <input class="form-control" placeholder="Enter your full name..." value="">
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label for="">Your Role</label>
-                                                <select class="form-control">
-                                                    <option>Web Developer</option>
-                                                    <option>Business Owner</option>
-                                                    <option>Other</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <div class="onboarding-slide">
-                            <div class="onboarding-media"><img alt="" src="/img/bigicon6.png" width="200px"></div>
-                            <div class="onboarding-content with-gradient">
-                                <h4 class="onboarding-title">Showcase App Features</h4>
-                                <div class="onboarding-text">In this example you can showcase some of the features of your application, it is very handy to make new users aware of your hidden features. You can use boostrap columns to split them up.</div>
-                                <div class="row">
-                                    <div class="col-sm-6">
-                                        <ul class="features-list">
-                                            <li>Fully Responsive design</li>
-                                            <li>Pre-built app layouts</li>
-                                            <li>Incredible Flexibility</li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <ul class="features-list">
-                                            <li>Boxed & Full Layouts</li>
-                                            <li>Based on Bootstrap 4</li>
-                                            <li>Developer Friendly </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="search-with-suggestions-w">
-            <div class="search-with-suggestions-modal">
-                <div class="element-search">
-                    <input class="search-suggest-input" placeholder="Start typing to search Evin...">
-                    <div class="close-search-suggestions"><i class="os-icon os-icon-x"></i></div>
-                </div>
-                <div class="search-suggestions-group">
-                    <div class="ssg-header">
-                        <div class="ssg-icon">
-                            <div class="os-icon os-icon-box"></div>
-                        </div>
-                        <div class="ssg-name">Projects</div>
-                        <div class="ssg-info">24 Total</div>
-                    </div>
-                    <div class="ssg-content">
-                        <div class="ssg-items ssg-items-boxed">
-                            <a class="ssg-item" href="users_profile_big.html">
-                                <div class="item-media" style="background-image: url(/img/company6.png)"></div>
-                                <div class="item-name">Integ<span>ration</span> with API</div>
-                            </a>
-                            <a class="ssg-item" href="users_profile_big.html">
-                                <div class="item-media" style="background-image: url(/img/company7.png)"></div>
-                                <div class="item-name">Deve<span>lopm</span>ent Project</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="search-suggestions-group">
-                    <div class="ssg-header">
-                        <div class="ssg-icon">
-                            <div class="os-icon os-icon-users"></div>
-                        </div>
-                        <div class="ssg-name">Customers</div>
-                        <div class="ssg-info">12 Total</div>
-                    </div>
-                    <div class="ssg-content">
-                        <div class="ssg-items ssg-items-list">
-                            <a class="ssg-item" href="users_profile_big.html">
-                                <div class="item-media" style="background-image: url(/img/avatar1.jpg)"></div>
-                                <div class="item-name">John Ma<span>yer</span>s</div>
-                            </a>
-                            <a class="ssg-item" href="users_profile_big.html">
-                                <div class="item-media" style="background-image: url(/img/avatar2.jpg)"></div>
-                                <div class="item-name">Th<span>omas</span> Mullier</div>
-                            </a>
-                            <a class="ssg-item" href="users_profile_big.html">
-                                <div class="item-media" style="background-image: url(/img/avatar3.jpg)"></div>
-                                <div class="item-name">Kim C<span>olli</span>ns</div>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="search-suggestions-group">
-                    <div class="ssg-header">
-                        <div class="ssg-icon">
-                            <div class="os-icon os-icon-folder"></div>
-                        </div>
-                        <div class="ssg-name">Files</div>
-                        <div class="ssg-info">17 Total</div>
-                    </div>
-                    <div class="ssg-content">
-                        <div class="ssg-items ssg-items-blocks">
-                            <a class="ssg-item" href="#">
-                                <div class="item-icon"><i class="os-icon os-icon-file-text"></i></div>
-                                <div class="item-name">Work<span>Not</span>e.txt</div>
-                            </a>
-                            <a class="ssg-item" href="#">
-                                <div class="item-icon"><i class="os-icon os-icon-film"></i></div>
-                                <div class="item-name">V<span>ideo</span>.avi</div>
-                            </a>
-                            <a class="ssg-item" href="#">
-                                <div class="item-icon"><i class="os-icon os-icon-database"></i></div>
-                                <div class="item-name">User<span>Tabl</span>e.sql</div>
-                            </a>
-                            <a class="ssg-item" href="#">
-                                <div class="item-icon"><i class="os-icon os-icon-image"></i></div>
-                                <div class="item-name">wed<span>din</span>g.jpg</div>
-                            </a>
-                        </div>
-                        <div class="ssg-nothing-found">
-                            <div class="icon-w"><i class="os-icon os-icon-eye-off"></i></div><span>No files were found. Try changing your query...</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
         <div class="layout-w">
             <!-- Loader -->
             <div class="bookshelf_wrapper" id="loader">
@@ -206,7 +56,7 @@
                 <div class="content-i">
                     @yield('content-box')
 
-                    @yield('content-panel')
+                    <!-- @yield('content-panel') -->
 
                 </div>
             </div>
