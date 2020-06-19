a9308bcd0a82110d15d3afb9ac416903a62a3e5c
Evin <toneevin07@gmail.com>
2020-06-19 23:33:48 +0300

    Added function in Notification Controller for sending genral email

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\SendMail;
+use App\Mail\GeneralSendMail;
 use Illuminate\Http\Response;
 use AfricasTalking\SDK\AfricasTalking;
 use App\Util;
@@ -16,6 +17,9 @@ class NotificationController extends Controller
         return view('mail.send_email');
     }
 
+    /**
+     * 
+     */
     function sendEmail(Request $request)
     {
         // $name = $request->input('name');
@@ -59,6 +63,118 @@ class NotificationController extends Controller
             return response()->json([
                 'success' => false,
                 'data'   => $e
+            ]);
+        }
+
+        if (count(Mail::failures()) > 0) {
+
+            foreach (Mail::failures() as $email_address) {
+                echo "$email_address";
+            }
+
+            return response()->json([
+                'success' => false,
+                'data'   => $email_address
+            ]);
+        }
+
+        return Response::json(array(
+            'success' => true,
+            'data'   => 'Mail Sent Successfully'
+        ));
+    }
+
+    /**
+     * 
+     */
+    function sendEmailGeneral(Request $request)
+    {
+        // var sendEmailURL = domainUrl + "/general/email/send/";
+        // var jsonEmailData = {}
+        // jsonEmailData = {
+        //     'message': emailMsg,
+        //     'to': buyer,
+        //     'subject': subject,
+        // };
+
+        $from = "bcapp@bc.com";
+
+        $data = array(
+            'message'   =>   $request->message,
+            'from'   =>   $from,
+            'subject'   =>   $request->subject
+        );
+
+        // dd($data);
+
+        try {
+            Mail::to($request->to)->send(new GeneralSendMail($data));
+
+            return response()->json([
+                'success' => true,
+                'data'   => 'Mail Sent Successfully!'
+            ]);
+        } catch (\Exception $e) {
+            // Error JSON
+            return response()->json([
+                'success' => false,
+                'data'   => $e
+            ]);
+        }
+
+        if (count(Mail::failures()) > 0) {
+
+            foreach (Mail::failures() as $email_address) {
+                echo "$email_address";
+            }
+
+            return response()->json([
+                'success' => false,
+                'data'   => $email_address
+            ]);
+        }
+
+        return Response::json(array(
+            'success' => true,
+            'data'   => 'Mail Sent Successfully'
+        ));
+    }
+
+    /**
+     * 
+     */
+    function sendEmailGeneralGet($message, $subject, $to)
+    {
+        // var sendEmailURL = domainUrl + "/general/email/send/";
+        // var jsonEmailData = {}
+        // jsonEmailData = {
+        //     'message': emailMsg,
+        //     'to': buyer,
+        //     'subject': subject,
+        // };
+
+        $from = "bcapp@bc.com";
+
+        $data = array(
+            'message'   =>   $message,
+            'from'   =>   $from,
+            'subject'   =>   $subject
+        );
+
+        // dd($data);
+
+        try {
+            Mail::to($to)->send(new GeneralSendMail($data));
+
+            return response()->json([
+                'success' => true,
+                'data'   => 'Mail Sent Successfully!'
+            ]);
+        } catch (\Exception $e) {
+            // Error JSON
+            return response()->json([
+                'success' => false,
+                'data'   => "Error =".$e
             ]);
         }
 
@@ -136,6 +252,40 @@ class NotificationController extends Controller
         $result   = $sms->send([
             'to'      => Util::smsNumber(),
             'message' => $request->message
+        ]);
+
+        // dd($result['status']);
+        // dd($result['data']);
+        return response()->json([
+            'success' => true,
+            'data'   => $result
+        ]);
+    }
+
+    /**
+     * Dynamic Sending SMS
+     */
+    function sendGeneralSMSGet($message)
+    {
+        // use 'sandbox' for development in the test environment
+        $username = 'sandbox'; 
+        
+        // use your sandbox app API key for development in the test environment
+        $apiKey   = Util::smsAPIKey();
+
+        $AT       = new AfricasTalking($username, $apiKey);
+
+        // Get one of the services
+        $sms      = $AT->sms();
+
+        // dd($request->message);
+
+        // $message = 'Congratulations! You have a new Order #'.  $request->order .'!';
+
+        // Use the service
+        $result   = $sms->send([
+            'to'      => Util::smsNumber(),
+            'message' => $message
         ]);
 
         // dd($result['status']);
