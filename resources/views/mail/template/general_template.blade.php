09b994e50daa9553fc1dab55452077f5cb7bf952
Evin <toneevin07@gmail.com>
2020-06-19 23:32:58 +0300

    Added mail general_template view for sending genral email

---
+++ resources/views/mail/template/general_template.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+
+<body style='background-color: #222533; padding: 20px; font-family: font-size: 14px; line-height: 1.43; font-family: "Helvetica Neue", "Segoe UI", Helvetica, Arial, sans-serif;'>
+    <div style="max-width: 600px; margin: 10px auto 20px; font-size: 12px; color: #a5a5a5; text-align: center;">
+        If you are unable to see this message, <a href="#" style="color: #a5a5a5; text-decoration: underline;">click here to view in browser</a>
+    </div>
+    <div style="max-width: 600px; margin: 0px auto; background-color: #fff; box-shadow: 0px 20px 50px rgba(0, 0, 0, 0.05);">
+        <table style="width: 100%;">
+            <tr>
+                <td style="background-color: #fff;"><img alt="" src="img/logo.png" style="width: 70px; padding: 20px;" /></td>
+                <!-- <td style="padding-left: 50px; text-align: right; padding-right: 20px;">
+                    <a href="#" style="color: #261d1d; text-decoration: underline; font-size: 14px; letter-spacing: 1px;">Sign In</a>
+                    <a href="#" style="color: #7c2121; text-decoration: underline; font-size: 14px; margin-left: 20px; letter-spacing: 1px;">Forgot Password</a>
+                </td> -->
+            </tr>
+        </table>
+        <img alt="" src="img/email-header-img.jpg" style="max-width: 100%; height: auto;" />
+        <div style="padding: 60px 70px;">
+            
+            <div style="color: #636363; font-size: 14px;margin-bottom: 20px;">
+                {{ $data['message'] }}
+               
+            </div>
+          
+            <div style="color: #636363; font-size: 14px; padding-top: 20px; border-top: 1px solid rgba(0, 0, 0, 0.05); margin-bottom: 50px;">
+                Thank you again for working with us. Join the Fight in combating the Counterfeit Industry.
+            </div>
+            <h4 style="margin-bottom: 10px;">Need Help?</h4>
+            <div style="color: #a5a5a5; font-size: 12px;">
+                <p>If you have any questions you can simply reply to this email or find our contact information below. Also contact us at <a href="#" style="text-decoration: underline; color: #4b72fa;">info@bc.com</a></p>
+            </div>
+        </div>
+        <div style="background-color: #f5f5f5; padding: 40px; text-align: center;">
+            <div style="margin-bottom: 20px;">
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/social-icons/twitter.png" style="width: 28px;" /></a>
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/social-icons/facebook.png" style="width: 28px;" /></a>
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/social-icons/linkedin.png" style="width: 28px;" /></a>
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/social-icons/instagram.png" style="width: 28px;" /></a>
+            </div>
+            <div style="margin-bottom: 20px;">
+                <a href="#" style="text-decoration: underline; font-size: 14px; letter-spacing: 1px; margin: 0px 15px; color: #261d1d;">Contact Us</a>
+                <a href="#" style="text-decoration: underline; font-size: 14px; letter-spacing: 1px; margin: 0px 15px; color: #261d1d;">Privacy Policy</a>
+                <a href="#" style="text-decoration: underline; font-size: 14px; letter-spacing: 1px; margin: 0px 15px; color: #261d1d;">Unsubscribe</a>
+            </div>
+            <div style="color: #a5a5a5; font-size: 12px; margin-bottom: 20px; padding: 0px 50px;">You are receiving this email because you signed up for Book Counterfeit. We use Book Counterfeit email system to send our emails</div>
+            <div style="margin-bottom: 20px;">
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/market-google-play.png" style="height: 33px;" /></a>
+                <a href="#" style="display: inline-block; margin: 0px 10px;"><img alt="" src="img/market-ios.png" style="height: 33px;" /></a>
+            </div>
+            <div style="margin-top: 20px; padding-top: 20px; border-top: 1px solid rgba(0, 0, 0, 0.05);">
+                <div style="color: #a5a5a5; font-size: 10px; margin-bottom: 5px;">Barclays Plaza Flr 12, Loita Street, Nairobi, Kenya</div>
+                <div style="color: #a5a5a5; font-size: 10px;">Copyright <script type="text/javascript"> document.write(new Date().getFullYear())</script> &copy; Book Counterfeit | All rights reserved.</div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
