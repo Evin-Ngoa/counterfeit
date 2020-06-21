4559a621b3806a86a925ee88acc05b488bc8b655
Evin <toneevin07@gmail.com>
2020-06-21 22:20:26 +0300

    Added modal for History of Customer scans

---
+++ resources/views/partials/transaction/customer_book_scan_status.blade.php
@@ -0,0 +1,30 @@
+<!-- Modal -->
+<div aria-hidden="true" class="onboarding-modal modal fade animated" id="customerBookScanStatusViewModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog modal-lg modal-centered" role="document">
+        <div class="modal-content text-center">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="os-icon os-icon-mail-14"></i> Traceability Report For Shipment Status View</h5>
+                <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true"> ×</span></button>
+            </div>
+            <div class="modal-body">
+                <!-- Stripped Tables -->
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-left">Customer ID</th>
+                                <th class="text-left">Points</th>
+                                <th class="text-left">participantInvoking</th>
+                                <th class="text-left">updatedAt</th>
+                                <th class="text-left">timestamp</th>
+                            </tr>
+                        </thead>
+                        <tbody id="customer_book_scan_status">
+                             
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
