1e01d3d34025851934f0d633b788aa0ee24a3bce
Evin <toneevin07@gmail.com>
2020-06-21 22:38:48 +0300

    Added map css

--- resources/views/layout/css.blade.php
+++ resources/views/layout/css.blade.php
@@ -1,2 +1,13 @@
     <link href="{{ asset("css/app-bc.css") }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset("icon_fonts_assets/typicons/typicons.css") }}" rel="stylesheet" type="text/css" />
+
+    <style>
+      #map {
+        width: 100%;
+        height: 400px;
+      }
+      #mapDash {
+        width: 100%;
+        height: 400px;
+      }
+    </style>
