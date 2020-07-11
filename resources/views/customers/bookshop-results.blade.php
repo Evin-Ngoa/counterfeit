7a5a9b0bcae71d6852a5a58296ea80ebb33229bf
Evin <toneevin07@gmail.com>
2020-07-11 20:53:26 +0300

    Added bookshop results view

---
+++ resources/views/customers/bookshop-results.blade.php
@@ -0,0 +1,24 @@
+<!-- Modal -->
+<div aria-hidden="true" class="onboarding-modal modal fade animated" id="bookShopViewModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog modal-lg modal-centered" role="document">
+        <div class="modal-content text-center">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="os-icon os-icon-mail-14"></i> BookShop Details</h5>
+                <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true"> ×</span></button>
+            </div>
+            <div class="modal-body">
+                <!-- Stripped Tables -->
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead id="bookshop_header">
+                            
+                        </thead>
+                        <tbody id="bookshop">
+                             
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
