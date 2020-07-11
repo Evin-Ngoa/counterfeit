1aa40c56ad1c5495a525d62429891da32f836a25
Evin <toneevin07@gmail.com>
2020-07-11 20:50:17 +0300

    Added book form dunction for bookshop form

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -28,6 +28,14 @@ class CustomerController extends Controller
     }
 
     /**
+     * Verifying Book Shops
+     */
+    public function verify_bookshop_form()
+    {
+       return view('customers.customer-form');
+    }
+
+    /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
