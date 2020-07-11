d6b725566dd2e38e4e0d856eba0f4b8c9eeffe2a
Evin <toneevin07@gmail.com>
2020-07-11 13:49:45 +0300

    Added modal for vieing book shipment historian supply

---
+++ resources/views/partials/transaction/shipment_book_ownership_trace.blade.php
@@ -0,0 +1,29 @@
+<!-- Modal -->
+<div aria-hidden="true" class="onboarding-modal modal fade animated" id="shipmentBookOwnershipStatusViewModal" role="dialog" tabindex="-1">
+    <div class="modal-dialog modal-lg modal-centered" role="document">
+        <div class="modal-content text-center">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="os-icon os-icon-mail-14"></i> Traceability Report For Ownership of a Book and Shipment</h5>
+                <button aria-label="Close" class="close" data-dismiss="modal" type="button"><span aria-hidden="true"> ×</span></button>
+            </div>
+            <div class="modal-body">
+                <!-- Stripped Tables -->
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th class="text-left">Shipment ID</th>
+                                <th class="text-left">Entity</th>
+                                <th class="text-left">participantInvoking</th>
+                                <th class="text-left">timestamp</th>
+                            </tr>
+                        </thead>
+                        <tbody id="shipment_book_ownership">
+                             
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
